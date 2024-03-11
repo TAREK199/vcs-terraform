abc1d08d6d483d50c642347d12b0bdbf5a8a8a44
rt.tech.54 <rt.tech.54@gmail.com>
2024-03-11 15:06:32 +0100

    first commit

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "apache" {
+  source          = "./terraform-aws-apache"
+
+  vpc_id          = var.vpc_id
+  subnet_id       = var.subnet_id
+  my_ip_with_cidr = var.my_ip_with_cidr
+  public_key      = var.public_key
+  instance_type   = var.instance_type
+  server_name     = var.server_name
+}
+
+output "public_ip" {
+  value = module.apache.public_ip
+}
---
+++ variables.tf
@@ -0,0 +1,24 @@
+
+variable "vpc_id" {
+    type = string
+}
+  
+variable "subnet_id" {
+    type = string
+}
+
+variable "my_ip_with_cidr" {
+    type = string
+}
+
+variable "public_key" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "server_name" {
+    type = string
+}
